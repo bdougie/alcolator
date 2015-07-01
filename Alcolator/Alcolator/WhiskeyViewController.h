1eee329f9e7481dae55642800fd1745be8e20b75
Brian Douglas <ilikerobot@gmail.com>
2015-06-30 23:31:39 -0700

    calculates

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Brian Douglas on 6/30/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
